b1e1bfa97142b28902da6eab483bd4f1dce3e147
Daniel_Zhang <shengjun.zhang@nyu.edu>
2021-04-28 16:28:25 -0500

    Add files via upload

--- fucntion_exclamation.jl
+++ fucntion_exclamation.jl
@@ -13,19 +13,25 @@ fucntion from Base.
 
 =#
 
-function my_fill!(v :: T, number :: Real) where T <: Vector
+function my_fill!(v :: T where T <: Vector, number :: Real) 
     v[:] = [number for i in 1:length(v)]
     return v
 end
 
-function my_fill_no_ex(v :: T, number :: Real) where T <: Vector
+function my_fill_no_ex(v :: T where T <: Vector, number :: Real) 
     v = [number for i in 1:length(v)]
     return v
 end
 
 v = [1.0, 1]
 
-println(v === my_fill!(v, 2)) # Physically the same v
-println(v === my_fill_no_ex(v, 3)) # Physically not the same v
+# Physically the same v
+println("The output of v === my_fill!(v, 2) is ", v === my_fill!(v, 2))
 
-println(v)
+# Physically not the same v
+println("The output of v === my_fill_no_ex(v, 3) is ", 
+        v === my_fill_no_ex(v, 3))
+
+u = my_fill_no_ex(v, 3)
+println("The output of u = my_fill_no_ex(v, 3) is ", u)
+println("v is ", v)
